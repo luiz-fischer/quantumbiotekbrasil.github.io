6fc3a5f0e038bc90e9cb97523b2b7f0bed6cc534
Luiz Fischer <luiz-fischer@hotmail.com>
2025-11-17 10:26:37 -0300

    fix: menu

--- js/script.js
+++ js/script.js
@@ -1,65 +1,57 @@
 function getBasePath() {
   const path = location.pathname;
   console.log('PATH', path)
-  const pathSegments = path.split("/");
+  const pathSegments = path.split("/").filter(seg => seg !== "");
   console.log(pathSegments)
-  // Remove o nome do arquivo e a pasta específica (se houver)
-  pathSegments.pop();
-
-  if (pathSegments[pathSegments.length - 1] === "contato") {
+  
+  // Remove o nome do arquivo se houver
+  if (pathSegments.length > 0 && pathSegments[pathSegments.length - 1].includes('.')) {
     pathSegments.pop();
   }
 
-  if (pathSegments[pathSegments.length - 1] === "empresa") {
-    pathSegments.pop();
-  }
-
-  if (pathSegments[pathSegments.length - 1] === "omniambiente") {
-    pathSegments.pop();
-  }
-
-  if (pathSegments[pathSegments.length - 1] === "produtos") {
-    pathSegments.pop();
-  }
-
-  if (pathSegments[pathSegments.length - 1] === "setores") {
+  // Remove a pasta específica se for uma subpasta
+  const lastSegment = pathSegments[pathSegments.length - 1];
+  if (["contato", "empresa", "omniambiente", "produtos", "setores"].includes(lastSegment)) {
     pathSegments.pop();
   }
 
   // Junta os segmentos de volta para obter o caminho base
-  const basePath = pathSegments.join("/");
+  // Adiciona / no início e no final
+  const basePath = pathSegments.length > 0 ? "/" + pathSegments.join("/") + "/" : "/";
 
+  console.log('BASE PATH:', basePath);
   return basePath;
 }
 
 function loadHeaderFooter() {
   const basePath = getBasePath();
 
-  const headerPath = new URL("header.html", location.origin + basePath)
-    .href;
-  const footerPath = new URL("footer.html", location.origin + basePath)
-    .href;
+  const headerPath = basePath + "header.html";
+  const footerPath = basePath + "footer.html";
+
+  console.log('Loading header from:', headerPath);
+  console.log('Loading footer from:', footerPath);
 
   fetch(headerPath)
-    .then((response) => response.text())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
     .then((data) => {
       document.getElementById("menu-container").innerHTML = data;
 
       // Ajustar caminhos de imagem
-      document.getElementById("logo").src = new URL(
-        "../assets/Logo Quantum Biotek-03.png",
-        location.origin + basePath
-      ).href;
+      document.getElementById("logo").src = basePath + "assets/Logo Quantum Biotek-03.png";
 
       // Ajustar caminhos dos links do menu-item
       const menuItems = document.querySelectorAll(".menu-item a");
       menuItems.forEach((item) => {
         const href = item.getAttribute("href");
         if (href && href.startsWith("./")) {
-          item.setAttribute(
-            "href",
-            new URL(href, location.origin + basePath).href
-          );
+          // Remove o ./ e adiciona o basePath
+          item.setAttribute("href", basePath + href.substring(2));
         }
       });
 
@@ -82,22 +74,20 @@ function loadHeaderFooter() {
     .catch((error) => console.error("Error loading the menu:", error));
 
   fetch(footerPath)
-    .then((response) => response.text())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
     .then((data) => {
       document.getElementById("footer-container").innerHTML = data;
 
       // Ajustar caminhos de imagem no footer
-      const footerBasePath = new URL(basePath, location.origin).href;
-      document.querySelector(".footer-logo").src = new URL(
-        "../assets/Logo-Omnienviro-quantum-biotek-.jpg",
-        footerBasePath
-      ).href;
-      document.querySelector(".footer-social img[src*='facebook']").src =
-        new URL("../icons/facebook.png", footerBasePath).href;
-      document.querySelector(".footer-social img[src*='twitter']").src =
-        new URL("../icons/twitter.png", footerBasePath).href;
-      document.querySelector(".footer-social img[src*='instagram']").src =
-        new URL("../icons/instagram.png", footerBasePath).href;
+      document.querySelector(".footer-logo").src = basePath + "assets/Logo-Omnienviro-quantum-biotek-.jpg";
+      document.querySelector(".footer-social img[src*='facebook']").src = basePath + "icons/facebook.png";
+      document.querySelector(".footer-social img[src*='twitter']").src = basePath + "icons/twitter.png";
+      document.querySelector(".footer-social img[src*='instagram']").src = basePath + "icons/instagram.png";
     })
     .catch((error) => console.error("Error loading the footer:", error));
 }
